b34902c88aa1703e91c60789024aea6c52f0dd0a
tassosvanis@outlook.com.gr <tassosvanis@outlook.com.gr>
2020-06-11 00:42:12 +0200

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import './App.css';
 
+import React from 'react';
+
+import {Switch, Route} from 'react-router-dom';
+
+import './App.css';
+import {BrowserRouter} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+    <div>
+        <h1>HATS PAGE</h1>
+    </div>
+);
+
 
 function App() {
   return (
-    <div>
-     <HomePage/>
-
-    </div>
+      <BrowserRouter>
+          <div>
+              <Switch>
+                <Route exact path='/' component={HomePage} />
+                <Route exact path='/shop/hats' component={HatsPage} />
+              </Switch>
+          </div>
+      </BrowserRouter>
   );
 }
 
