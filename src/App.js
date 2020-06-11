64681b76aaf4269690a687cc80d6f371f61fb520
tassosvanis@outlook.com.gr <tassosvanis@outlook.com.gr>
2020-06-12 00:56:10 +0200

    update app with shop page, collection preview and collection list page

--- src/App.js
+++ src/App.js
@@ -6,12 +6,7 @@ import {Switch, Route} from 'react-router-dom';
 import './App.css';
 import {BrowserRouter} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
-
-const HatsPage = () => (
-    <div>
-        <h1>HATS PAGE</h1>
-    </div>
-);
+import ShopPage from "./pages/shop/shop.component";
 
 
 function App() {
@@ -20,7 +15,7 @@ function App() {
           <div>
               <Switch>
                 <Route exact path='/' component={HomePage} />
-                <Route exact path='/shop/hats' component={HatsPage} />
+                <Route exact path='/shop' component={ShopPage} />
               </Switch>
           </div>
       </BrowserRouter>
