357dba6c5f8ccc19f42d04633e7d56f218c40bda
tassosvanis@outlook.com.gr <tassosvanis@outlook.com.gr>
2020-06-12 18:50:51 +0200

    adding custom button

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import {BrowserRouter} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from "./pages/shop/shop.component";
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+
 
 function App() {
   return (
@@ -17,6 +19,7 @@ function App() {
               <Switch>
                 <Route exact path='/' component={HomePage} />
                 <Route exact path='/shop' component={ShopPage} />
+                  <Route exact path='/signin' component={SignInAndSignUpPage} />
               </Switch>
           </div>
       </BrowserRouter>
