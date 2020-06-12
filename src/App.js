3e696ad3ddcd2e0d0d920e07b2bf0f699b23484a
tassosvanis@outlook.com.gr <tassosvanis@outlook.com.gr>
2020-06-12 17:38:29 +0200

    added header

--- src/App.js
+++ src/App.js
@@ -7,12 +7,13 @@ import './App.css';
 import {BrowserRouter} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from "./pages/shop/shop.component";
-
+import Header from './components/header/header.component';
 
 function App() {
   return (
       <BrowserRouter>
           <div>
+              <Header />
               <Switch>
                 <Route exact path='/' component={HomePage} />
                 <Route exact path='/shop' component={ShopPage} />
