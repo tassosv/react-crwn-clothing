aa30514e8138d8a72c6b8070823a93309d8fc0ab
tassosvanis@outlook.com.gr <tassosvanis@outlook.com.gr>
2020-06-14 12:14:14 +0200

    implemented fb utils + ability to auth user with mail and save them to our db

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from "./pages/shop/shop.component";
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 
 class App extends React.Component {
@@ -23,8 +23,21 @@ class App extends React.Component {
     unsubscribeFromAuth = null;
 
     componentDidMount() {
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-            this.setState({currentUser: user});
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+            if(userAuth){
+                const userRef = await createUserProfileDocument(userAuth);
+
+                userRef.onSnapshot(snapshot => {
+                    this.setState({
+                        currentUser: {
+                            id : snapshot.id,
+                            ...snapshot.data()
+                        }
+                    });
+                });
+
+            }
+            this.setState({currentUser: userAuth});
         });
     }
 
